c3e567216e777c3b9446c97528d754dd40566f53
omar944 <omaralbitar77@gmail.com>
2021-10-09 00:39:57 +0300

    update build

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer()
 {
     return <div className="container-fluid mt-4 p-0">
         <div className ="bg-white shadow-sm p-3 d-flex justify-content-center align-items-center ">
-            <span className=" tajawal footer-text text-center">تم تصميم الموقع بشكل بسيط ، ان كان لديك اهتمام في تحسينه لا تتردد في <a className="contact" href="#">الاتصال</a></span>
+            <span className=" tajawal footer-text text-center">تم تصميم الموقع بشكل بسيط ، ان كان لديك اهتمام في تحسينه لا تتردد في <a className="contact" href="https://t.me/YahyaZ99">الاتصال</a></span>
         </div>
     </div>;
 }
